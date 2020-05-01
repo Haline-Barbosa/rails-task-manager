170ef12344f91bf9dfb427ebccfdc9887d8b6218
Haline Barbosa <barbosahaline@gmail.com>
2020-05-01 13:09:03 +0100

    Definindo rotas e acoes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,34 @@
 class TasksController < ApplicationController
+  def index
+    @songs = Song.all
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new(params[:song])
+    @song.save!
+  end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def destroy
+    @song = Song.find(params[:id])
+    @song.destroy
+
+    redirect_to songs_path
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def update
+    @song = Song.find(params[:id])
+    @song.update(params[:song])
+  end
 end
